8a65237c6be4f15b49b9bfb3a08c8e10143a3267
Qiang <qxl0@yahoo.com>
2022-02-18 13:30:43 -0500

    Initial check in

---
+++ deploy.py
@@ -0,0 +1,60 @@
+import json
+from solcx import compile_standard, install_solc
+from web3 import Web3
+import os
+
+install_solc("0.6.0")
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x26361B3EC5878e84f966210Ba2Bb9678f0333171"
+private_key = os.getenv("PRIVATE_KEY")
+print(private_key)
+# create contract
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print(SimpleStorage)
+# get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
+# build trx
+# sign trx
+# send a trx
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+# print(signed_txn)
