1e14525e4b19b6a161e12dbedbfaf41cac66e25b
Qiang <qxl0@yahoo.com>
2022-02-18 20:22:56 -0500

    Another way of reading private_key

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,10 +1,11 @@
-from brownie import accounts
+from brownie import accounts, config
 import os
 
 
 def deploy_simple_storage():
     # account = accounts.load("qiang-account")
-    account = accounts.add(os.getenv("PRIVATE_KEY"))
+    # account = accounts.add(os.getenv("PRIVATE_KEY"))
+    account = accounts.add(config["wallets"]["from_key"])
     print(account)
 
 
