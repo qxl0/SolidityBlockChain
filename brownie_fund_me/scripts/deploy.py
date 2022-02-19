d437bd25b2196ecbac20a11107f3fdbc8ebe5087
Qiang <qxl0@yahoo.com>
2022-02-19 14:48:43 -0500

    Added network support (live vs mock)

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,11 +1,26 @@
-from brownie import FundMe
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    print(account)
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+
+    # if we are on a persistant network like rinkeby, use the associated address
+    # othrwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"The actiev network is {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mocks Deployed!")
+
+    fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=True)
     print(f"Contract deployed to {fund_me.address}")
 
 
