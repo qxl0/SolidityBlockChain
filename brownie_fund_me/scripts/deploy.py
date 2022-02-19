d6c766c9e242b482ec55b48132d093e0a9f9c477
Qiang <qxl0@yahoo.com>
2022-02-19 15:34:34 -0500

    Set up yaml,env and projects to work with ganache-local

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,5 +1,9 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account, deploy_mocks
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
@@ -7,7 +11,7 @@ def deploy_fund_me():
 
     # if we are on a persistant network like rinkeby, use the associated address
     # othrwise, deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -1,9 +1,13 @@
 from brownie import accounts, MockV3Aggregator, network, config
 from web3 import Web3
 
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+DECIMALS = 18
+STARTING_PRICE = 2000
+
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
@@ -13,6 +17,6 @@ def deploy_mocks():
     print(f"The actiev network is {network.show_active()}")
     print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(18, Web3.toWei(2000, "ether"), {"from": get_account()})
-    price_feed_address = MockV3Aggregator[-1].address
-    print("Mocks Deployed!")
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+        )
