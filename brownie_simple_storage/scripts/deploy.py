1a8796f2c29bdd661611df0479a2d835c7ee28c8
Qiang <qxl0@yahoo.com>
2022-02-18 20:12:06 -0500

    Added account private key through .env

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,8 +1,10 @@
 from brownie import accounts
+import os
 
 
 def deploy_simple_storage():
-    account = accounts.load("qiang-account")
+    # account = accounts.load("qiang-account")
+    account = accounts.add(os.getenv("PRIVATE_KEY"))
     print(account)
 
 
