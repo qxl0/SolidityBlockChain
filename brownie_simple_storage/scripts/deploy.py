690faccaaebc4722e4702f33eb50b1943b3fa778
Qiang <qxl0@yahoo.com>
2022-02-18 20:35:13 -0500

    Addes unit tests

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -7,7 +7,12 @@ def deploy_simple_storage():
     # account = accounts.add(os.getenv("PRIVATE_KEY"))
     account = accounts.add(config["wallets"]["from_key"])
     simple_storage = SimpleStorage.deploy({"from": account})
-    print(simple_storage)
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(13, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 
 def main():
